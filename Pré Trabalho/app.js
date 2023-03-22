ca4d63baec70cbdb652abdd0d9b620631baf0a34
Sara Anita de Sá <sara.anita.desa@gmail.com>
2023-03-21 22:33:22 -0300

    Comentando o else

--- Pré Trabalho/app.js
+++ Pré Trabalho/app.js
@@ -12,9 +12,12 @@ type="text/javascript">
                 document.write("<p>Legal, Eu sou de Touro também!</p>");
               }
             
-            else {
-                alert(`Signo: ${signo}`)
-            }
+            // else {
+            //     alert(`Signo: ${signo}`)
+            // }
 
             if(signo.toLocaleLowerCase() === "cancer"){
                 alert(`${signo}... hmmm entao você é dramatico`)}
+
+            if (signo.toLocaleLowerCase() === "aries") {
+              alert(`${signo} ... Ou você é o João amor da minha vida ou você é o proprio Capeta`)}
